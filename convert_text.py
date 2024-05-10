af1fb8763dad9b8827c415678dd9e28ade3f44bf
Joshua Penn <joshualeepenn@gmail.com>
2024-05-10 13:39:35 -0400

    Issue #2 - Python function to convert text to mocking
    - Added convert_test.py for conversion function
    - Added tests
    - Updated README to mention case folding limitations

---
+++ convert_text.py
@@ -0,0 +1,16 @@
+def convert_text(input_text, start_upper_case = None):
+    if start_upper_case == None:
+        start_upper_case = True
+    
+    outputText = ""
+    for index in range(len(input_text)):
+
+        # If either: (character index is even and start upper case) or (character is odd and start lower case)
+        if ((not index % 2 and start_upper_case) or (index % 2 and not start_upper_case)):
+            outputText = outputText + input_text[index].upper()
+        
+        # Else, by defalt, either: (character index is even and start lower case) or (character is odd and start upper case)
+        else:
+            outputText = outputText + input_text[index].lower()
+    
+    return outputText
