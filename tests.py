ab61260d94e07d5c971784601ee4ccbe9bc517e2
Joshua Penn <joshualeepenn@gmail.com>
2024-05-25 18:20:43 -0400

    MISC - use naming conventions for constnats

--- tests.py
+++ tests.py
@@ -1,50 +1,50 @@
 import unittest
 from convert_text import convert_text, lambda_handler, JSON_KEY_INPUT_TEXT, JSON_KEY_START_UPPER_CASE
 
-input_text = "Money can't buy happiness."
-expected_output_start_upper_case = "MoNeY CaN'T BuY HaPpInEsS."
-expected_output_start_lower_case = "mOnEy cAn't bUy hApPiNeSs."
-expected_output_lambda = "{\"convertedText\": \"" + expected_output_start_upper_case + "\"}"
-conversion_error_message = "Conversion error:"
-lambda_error_message = "Lambda error:"
+INPUT_TEXT = "Money can't buy happiness."
+EXPECTED_OUTPUT_START_UPPER_CASE = "MoNeY CaN'T BuY HaPpInEsS."
+EXPECTED_OUTPUT_START_LOWER_CASE = "mOnEy cAn't bUy hApPiNeSs."
+EXPECTED_OUTPUT_LAMBDA = "{\"convertedText\": \"" + EXPECTED_OUTPUT_START_UPPER_CASE + "\"}"
+CONVERSION_ERROR_MESSAGE = "Conversion error:"
+LAMBDA_ERROR_MESSAGE = "Lambda error:"
 
 
 class TestCalculations(unittest.TestCase):
     def test_default(self):
-        output_text = convert_text(input_text)
-        self.assertEqual(expected_output_start_upper_case, output_text), conversion_error_message
+        output_text = convert_text(INPUT_TEXT)
+        self.assertEqual(EXPECTED_OUTPUT_START_UPPER_CASE, output_text), CONVERSION_ERROR_MESSAGE
 
     def test_upper(self):
-        output_text = convert_text(input_text, True)
-        self.assertEqual(expected_output_start_upper_case, output_text, conversion_error_message)
+        output_text = convert_text(INPUT_TEXT, True)
+        self.assertEqual(EXPECTED_OUTPUT_START_UPPER_CASE, output_text, CONVERSION_ERROR_MESSAGE)
 
     def test_lower(self):
-        output_text = convert_text(input_text, False)
-        self.assertEqual(expected_output_start_lower_case, output_text, conversion_error_message)
+        output_text = convert_text(INPUT_TEXT, False)
+        self.assertEqual(EXPECTED_OUTPUT_START_LOWER_CASE, output_text, CONVERSION_ERROR_MESSAGE)
 
     # If trimmed AFTER conversion, the case will be opposite due whitespace being indexed
     def test_string_trim_odd_number_of_spaces(self):
-        output_text = convert_text("   " + input_text + "   ", True)
-        self.assertEqual(expected_output_start_upper_case, output_text, conversion_error_message)
+        output_text = convert_text("   " + INPUT_TEXT + "   ", True)
+        self.assertEqual(EXPECTED_OUTPUT_START_UPPER_CASE, output_text, CONVERSION_ERROR_MESSAGE)
 
     def test_string_trim_even_number_of_spaces(self):
-        output_text = convert_text("    " + input_text + "    ", True)
-        self.assertEqual(expected_output_start_upper_case, output_text, conversion_error_message)
+        output_text = convert_text("    " + INPUT_TEXT + "    ", True)
+        self.assertEqual(EXPECTED_OUTPUT_START_UPPER_CASE, output_text, CONVERSION_ERROR_MESSAGE)
 
     def test_string_trim_newline(self):
-        output_text = convert_text("\n" + input_text + "\n", True)
-        self.assertEqual(expected_output_start_upper_case, output_text, conversion_error_message)
+        output_text = convert_text("\n" + INPUT_TEXT + "\n", True)
+        self.assertEqual(EXPECTED_OUTPUT_START_UPPER_CASE, output_text, CONVERSION_ERROR_MESSAGE)
 
     def test_string_trim_tab(self):
-        output_text = convert_text("\t" + input_text + "\t", True)
-        self.assertEqual(expected_output_start_upper_case, output_text, conversion_error_message)
+        output_text = convert_text("\t" + INPUT_TEXT + "\t", True)
+        self.assertEqual(EXPECTED_OUTPUT_START_UPPER_CASE, output_text, CONVERSION_ERROR_MESSAGE)
 
     def test_lambda_handler(self):
         event = {
-            JSON_KEY_INPUT_TEXT: input_text,
+            JSON_KEY_INPUT_TEXT: INPUT_TEXT,
             JSON_KEY_START_UPPER_CASE: True
         }
-        self.assertEqual(expected_output_lambda, lambda_handler(event, None), lambda_error_message)
+        self.assertEqual(EXPECTED_OUTPUT_LAMBDA, lambda_handler(event, None), LAMBDA_ERROR_MESSAGE)
 
 
 if __name__ == '__main__':
