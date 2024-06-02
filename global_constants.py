bd8f9114fbeed7113d105e7096dbf5f62fa398c0
Joshua Penn <joshualeepenn@gmail.com>
2024-06-02 14:18:04 -0400

    #19 env var for acao header

---
+++ global_constants.py
@@ -0,0 +1,17 @@
+# request payload keys
+JSON_KEY_INPUT_TEXT = "inputText"
+JSON_KEY_START_UPPER_CASE = "startUpperCase"
+
+# response payload keys
+STATUS_CODE_KEY = "statusCode"
+IS_BASE64_ENCODED_KEY = "isBase64Encoded"
+HEADERS_KEY = "headers"
+CONTENT_TYPE_HEADER_KEY = "Content-Type"
+CONTENT_TYPE_APPLICATION_JSON = "application/json"
+ACAO_HEADER_KEY = "Access-Control-Allow-Origin"
+BODY_KEY = "body"
+JSON_KEY_CONVERTED_TEXT = "convertedText"
+
+# ACAO env var
+ACAO_ENV_VAR = "MOCKING_TEXT_LAMBDA_ACAO"
+ACAO_ENV_VAR_NOT_SET_ERROR_MESSAGE = ACAO_HEADER_KEY + "header is not set on the server"
