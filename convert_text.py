9dc33cb9322888d596b21ba7d53e57d85700c9bf
Joshua Penn <joshualeepenn@gmail.com>
2024-06-01 20:36:31 -0400

    #17 add comments

--- convert_text.py
+++ convert_text.py
@@ -23,21 +23,28 @@ def convert_text(input_text, start_upper_case=None):
     if start_upper_case is None:
         start_upper_case = True
 
+    # remove leading and trailing whitespace chars
     input_text = input_text.strip()
 
+    # string to construct output from converted input
     output_text = str()
 
-    # Boolean to toggle for every letter
+    # boolean to toggle the expected case
     next_char_upper = start_upper_case
 
     for index in range(len(input_text)):
+        # add whitespace to constructed string without toggling the expected case
         if input_text[index].isspace():
             output_text = output_text + input_text[index]
             continue
+
+        # add char to constructed string based on the expected case
         if next_char_upper:
             output_text = output_text + input_text[index].upper()
         else:
             output_text = output_text + input_text[index].lower()
+
+        # toggle the expected case
         next_char_upper = not next_char_upper
 
     return output_text
