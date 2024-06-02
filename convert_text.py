f7f009bb14c9d9c1b72035044b5ad33757156a0e
Joshua Penn <joshualeepenn@gmail.com>
2024-06-01 20:22:50 -0400

    #17 skip whitespace when converting

--- convert_text.py
+++ convert_text.py
@@ -23,19 +23,21 @@ def convert_text(input_text, start_upper_case=None):
     if start_upper_case is None:
         start_upper_case = True
 
-    # remove leading and trailing whitespace chars
     input_text = input_text.strip()
 
     output_text = str()
+
+    # Boolean to toggle for every letter
+    next_char_upper = start_upper_case
+
     for index in range(len(input_text)):
-
-        # If either: (character index is even and start upper case) or (character is odd and start lower case)
-        # Then make it upper case
-        if (not index % 2 and start_upper_case) or (index % 2 and not start_upper_case):
+        if input_text[index].isspace():
+            output_text = output_text + input_text[index]
+            continue
+        if next_char_upper:
             output_text = output_text + input_text[index].upper()
-
-        # Else, make it lower case
         else:
             output_text = output_text + input_text[index].lower()
+        next_char_upper = not next_char_upper
 
     return output_text
